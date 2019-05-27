a8895407e1f8e1a1265f4e5a0b20c20a303956f1
Jonathan Regis <ekoungouloujonathan@gmail.com>
2019-05-27 11:53:23 -0700

    Added build folder containing static files

---
+++ build/precache-manifest.af2bc5107bc1b304be803d1204dad81f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e294c3f0a10ae4e79531cf68026c976c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88ffea04f91ebed900ec",
+    "url": "/static/css/main.ebd2a11a.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "88ffea04f91ebed900ec",
+    "url": "/static/js/main.cd0eff65.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
